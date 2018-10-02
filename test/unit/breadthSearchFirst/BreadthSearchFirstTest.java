c9b2a225c52c284c4ee1b1107436d76a2d13c7cc
Raj <32778886+rk16449@users.noreply.github.com>
2018-10-02 18:39:40 +0100

    updated; classpath to include JUnit

---
+++ test/unit/breadthSearchFirst/BreadthSearchFirstTest.java
@@ -0,0 +1,14 @@
+package unit.breadthSearchFirst;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BreadthSearchFirstTest {
+
+	@Test
+	public void testCreateBFS() {
+		fail("Not yet implemented");
+	}
+
+}
