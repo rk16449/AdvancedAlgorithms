77c93eaaeec54ead97a1ceee4a6c9c7119536dd3
Raj <32778886+rk16449@users.noreply.github.com>
2018-10-02 18:29:52 +0100

    updated; Node.java

--- src/breadthSearchFirst/Node.java
+++ src/breadthSearchFirst/Node.java
@@ -6,10 +6,43 @@ import java.util.List;
 public class Node<T> {
 	private T data;
 	private boolean visited;
-	private List<Node> childNodes; 
-	
-	public Node(T data){
+	private List<Node<T>> childNodes;
+
+	public Node(T data) {
 		this.data = data;
-		childNodes = new ArrayList<Node>();
+		childNodes = new ArrayList<>();
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public boolean getVisited() {
+		return this.visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	public List<Node<T>> getChildren() {
+		return childNodes;
+	}
+
+	public void setChildren(List<Node<T>> childList) {
+		this.childNodes = childList;
+	}
+
+	public void addChildNode(Node<T> child) {
+		this.childNodes.add(child);
+	}
+
+	@Override
+	public String toString() {
+		return "" + this.data;
 	}
 }
