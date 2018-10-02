f7d88d226f9653c85e4db150d3e8dbc62d5d0523
Raj <32778886+rk16449@users.noreply.github.com>
2018-10-02 19:40:43 +0100

    updated; DepthFirstSearch

--- src/depthFirstSearch/DepthFirstSearch.java
+++ src/depthFirstSearch/DepthFirstSearch.java
@@ -1,5 +1,35 @@
 package depthFirstSearch;
 
-public class DepthFirstSearch {
+import java.util.Stack;
 
+public class DepthFirstSearch<T> {
+	// Using a stack data structure
+	private Stack<Node<T>> stack;
+	
+	public DepthFirstSearch(){
+		this.stack = new Stack<>();
+	}
+	
+	public void dfs(Node<T> root){
+		
+		stack.add(root);
+		root.setVisited(true);
+		
+		while(!stack.isEmpty()){
+			
+			// grab the node
+			Node<T> node = stack.pop();
+			
+			// Loop through its children
+			for(Node<T> n : node.getChildren()){
+				
+				// check that we haven't visited this before
+				if(!n.getVisited()){
+					n.setVisited(true);
+					// add to the stack
+					stack.push(n);
+				}
+			}
+		}
+	}
 }
--- src/depthFirstSearch/Node.java
+++ src/depthFirstSearch/Node.java
@@ -1,27 +1,48 @@
 package depthFirstSearch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node<T> {
-	
 	private T data;
-	private boolean isVisited;
-	
-	public Node(T data){
+	private boolean visited;
+	private List<Node<T>> childNodes;
+
+	public Node(T data) {
 		this.data = data;
+		childNodes = new ArrayList<>();
 	}
-	
-	public T getData(){
+
+	public T getData() {
 		return data;
 	}
-	
-	public void setData(T data){
+
+	public void setData(T data) {
 		this.data = data;
 	}
-	
-	public boolean getVisited(){
-		return isVisited;
+
+	public boolean getVisited() {
+		return this.visited;
 	}
-	
-	public void setVisited(boolean isVisited){
-		this.isVisited = isVisited;
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	public List<Node<T>> getChildren() {
+		return childNodes;
+	}
+
+	public void setChildren(List<Node<T>> childList) {
+		this.childNodes = childList;
+	}
+
+	public void addChildNode(Node<T> child) {
+		this.childNodes.add(child);
+	}
+
+	@Override
+	public String toString() {
+		return "" + this.data;
 	}
 }
